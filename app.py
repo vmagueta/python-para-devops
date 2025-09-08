8d935723e0bae491332527ab14ce7d7802a1e895
vmagueta <vs@platforme.com>
2025-09-08 13:12:06 +0100

    Aula de virtual environments

---
+++ app.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.insert(0, "custom")
+
+import config
+print(config.VERSION)
---
+++ meu_app/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from meu-app!")
+
+
+if __name__ == "__main__":
+    main()
